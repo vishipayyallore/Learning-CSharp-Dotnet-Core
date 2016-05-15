62b1ed8066d0c87fcf1e5b767e778f466ef61191
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-05-15 20:20:36 +0530

    Simple Class Project

---
+++ hwcliconsole/HelloWorldCli.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class HelloWorldCli
+    {
+        public static void Main(string[] args)
+        {
+            var user = new Person { Name = "Shiva Sai", Age = 26 };
+            
+            Console.WriteLine("User Details ....");
+            Console.WriteLine($"Name: {user.Name}\nAge: {user.Age}");
+            
+            Console.ReadLine();
+        }
+    }
+}
