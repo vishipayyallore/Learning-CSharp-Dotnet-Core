1bf0d3b99580e68b4df2aec9c7a05390493d7fd9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-05-18 22:41:53 +0530

    Initial Version

---
+++ hwsqlite/MainCls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+            
+            WriteLine("Hello World!");
+            
+            WriteLine("\nPress any key ...");
+            ReadLine();
+        }
+    }
+}
