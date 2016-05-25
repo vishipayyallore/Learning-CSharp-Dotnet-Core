a454b9c0a6323458ffd0206852f9fddb4f60eca6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-05-25 20:48:29 +0530

    A

--- hwapp/Program.cs
+++ hwapp/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿
+using System;
 
-namespace ConsoleApplication
+namespace hwapp
 {
     public class Program
     {
---
+++ hwsqlite/Migrations/20160519144639_winsqlite1.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using hwsqlite.Sample;
+
+namespace hwsqlite.Migrations
+{
+    [DbContext(typeof(eCommerceDbContext))]
+    [Migration("20160519144639_winsqlite1")]
+    partial class winsqlite1
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901");
+
+            modelBuilder.Entity("hwsqlite.Sample.Product", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<bool>("IsAvailable");
+
+                    b.Property<string>("Name");
+
+                    b.Property<decimal>("Price");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
