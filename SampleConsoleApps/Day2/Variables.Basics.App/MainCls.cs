f029492f95894e432b68f494b54d428f531cec86
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-05-31 22:49:56 +0530

    Added few basic program on Variables.

---
+++ SampleConsoleApps/Day2/Variables.Basics.App/MainCls.cs
@@ -0,0 +1,20 @@
+﻿using Variables.Basics;
+using static System.Console;
+
+namespace Variables.Basics.App
+{
+
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = System.ConsoleColor.Cyan;
+            var variablesDemo1 = new VariablesDemo1Cls();
+            variablesDemo1.DisplayDetails();
+
+            WriteLine("\n\nPress any key ...");
+            ReadLine();
+        }
+    }
+
+}
---
+++ SampleConsoleApps/Day2/Variables.Basics/VariablesDemo1Cls.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+
+namespace Variables.Basics
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class VariablesDemo1Cls
+    {
+
+        #region Variables.
+
+        private const int Population = 66000000;
+        private const double Weight = 1.88;
+        private const decimal Price = 4.99M;
+        private const string Fruit = "Apples";
+        private char _letter = 'S';
+        private bool _isHappy = true;
+
+        private const string HeaderDoubleDash = "===================================================";
+        private const string HeaderSingleDash = "---------------------------------------------------";
+        #endregion
+
+        public VariablesDemo1Cls()
+        {
+        }
+
+        #region Methods
+        public void DisplayDetails()
+        {
+            WriteLine(HeaderDoubleDash);
+            WriteLine("Information:");
+            WriteLine(HeaderSingleDash);
+            WriteLine($"The Population of UK is {Population:N0}");
+            WriteLine($"{Weight} Kgs of {Fruit} costs {Price:C}");
+            WriteLine($"Your favorite character is {_letter}; your life is Happy and you are a {_isHappy} person.");
+            WriteLine(HeaderDoubleDash);
+        }
+        #endregion
+    }
+
+}
