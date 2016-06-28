5e9920154e29b7b0c0ea44f489afbdb7226c4045
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-06-28 21:43:03 +0530

    Updated to 1.0.0-rc2-3002702

---
+++ hwappwincli/MainCls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace hwappwincli
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Cyan;
+
+            WriteLine("Hello World! \n From Windows .Net CLI.");
+
+            WriteLine("\n\nPress any key ....");
+            ReadLine();
+        }
+    }
+
+}
