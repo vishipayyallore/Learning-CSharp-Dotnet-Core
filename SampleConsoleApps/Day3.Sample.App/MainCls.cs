27a0aa6750622210fdd111a3f2b672e5e081c068
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-08-30 17:34:39 +0530

    AAA

--- SampleConsoleApps/Day3.Sample.App/MainCls.cs
+++ SampleConsoleApps/Day3.Sample.App/MainCls.cs
@@ -1,9 +1,21 @@
-﻿namespace Day3.Sample.App
+﻿using System;
+
+namespace Day3.Sample.App
 {
     public class MainCls
     {
         public static void Main(string[] args)
         {
+            int startValue = Int32.Parse(args[0]);
+            int endValue = 10;
+
+            if (args.Length == 2)
+            {
+                endValue = Int32.Parse(args[1]);
+            }
+
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
         }
     }
 }
