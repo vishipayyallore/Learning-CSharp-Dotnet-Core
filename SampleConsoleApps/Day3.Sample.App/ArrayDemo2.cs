c5472739d001db4191180e0cf1a25c8d9d2dddb7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-02 23:26:38 +0530

    Complete the Program

--- SampleConsoleApps/Day3.Sample.App/ArrayDemo2.cs
+++ SampleConsoleApps/Day3.Sample.App/ArrayDemo2.cs
@@ -13,16 +13,25 @@ namespace Day3.Sample.App
             Console.WriteLine("Enter the number of Repetations:");
             var numberOfArray = Console.ReadLine().Trim();
             var rows = int.Parse(numberOfArray);
-            var count = new Dictionary<string, int>();
+            var sum = 0;
+            var count = new Dictionary<int, int>();
 
             for (var iCtr = 0; iCtr < rows; iCtr++)
             {
                 Console.WriteLine("Enter the number of Elements:");
                 var numberOfElements = int.Parse(Console.ReadLine().Trim());
+                sum += numberOfElements;
                 Console.WriteLine("Enter the Values for Array:");
                 var arrayValues = Console.ReadLine().Trim().Split(' ');
                 var dataArray = arrayValues.Select(int.Parse).ToArray();
+                count[iCtr] = dataArray.Where(e => e > 0).Sum();
             }
+
+            foreach (var keyValuePair in count)
+            {
+                Console.WriteLine($"Number Sum = {sum} :: Array Sum = {keyValuePair.Value}");
+            }
+            
         }
 
     }
