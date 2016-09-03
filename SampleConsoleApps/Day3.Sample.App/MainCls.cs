7ee82aac362d340f257189a5bff4733f1ef73c42
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-03 22:20:28 +0530

    Logical Program

--- SampleConsoleApps/Day3.Sample.App/MainCls.cs
+++ SampleConsoleApps/Day3.Sample.App/MainCls.cs
@@ -7,6 +7,15 @@ namespace Day3.Sample.App
         public static void Main(string[] args)
         {
 
+            Console.WriteLine("Enter the number of Elements:");
+            var numberOfElements = int.Parse(Console.ReadLine().Trim());
+            var arrayElements = new int[numberOfElements];
+            for (var iCtr = 0; iCtr < numberOfElements; iCtr++)
+            {
+                Console.WriteLine($"Enter the {iCtr+1} Element:");
+                arrayElements[iCtr] = int.Parse(Console.ReadLine().Trim());
+            }
+
             ArrayDemo2.Run();
 
             ArrayDemo1.Run();
