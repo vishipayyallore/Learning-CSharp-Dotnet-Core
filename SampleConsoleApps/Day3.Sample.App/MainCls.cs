8b3cab0ce4d43ae7212b37e7fda67ad5a0d51a34
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-03 23:14:47 +0530

    Another Logical Program

--- SampleConsoleApps/Day3.Sample.App/MainCls.cs
+++ SampleConsoleApps/Day3.Sample.App/MainCls.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day3.Sample.App
 {
@@ -6,6 +7,40 @@ namespace Day3.Sample.App
     {
         public static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter the number of Elements:");
+            var numberOfElements = int.Parse(Console.ReadLine().Trim());
+            var callingOrder = new int[numberOfElements];
+            var idealOrder = new int[numberOfElements];
+            var lastIndex = numberOfElements - 1;
+
+            Console.WriteLine("Enter Elements for Calling Order:");
+            var arrayValues = Console.ReadLine().Trim().Split(' ');
+            callingOrder = arrayValues.Select(int.Parse).ToArray();
+            Console.WriteLine("Enter Elements for Ideal Order:");
+            arrayValues = null;
+            arrayValues = Console.ReadLine().Trim().Split(' ');
+            idealOrder = arrayValues.Select(int.Parse).ToArray();
+
+            
+            for (var iCtr = 0; iCtr < numberOfElements; iCtr++)
+            {
+                for (var jCtr = iCtr; jCtr < numberOfElements; jCtr++)
+                {
+                    if (idealOrder[iCtr] != callingOrder[iCtr])
+                    {
+                        var temp = callingOrder[lastIndex];
+                        Array.Copy(callingOrder, 0, callingOrder, 1, callingOrder.Length - 1);
+                        callingOrder[lastIndex] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    Console.WriteLine($" {idealOrder[iCtr]} --- {callingOrder[jCtr]}");
+                }
+                Console.WriteLine();
+            }
 
             SingleDArrayReverse.Run();
 
