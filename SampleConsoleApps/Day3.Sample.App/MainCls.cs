37f4114f05e09a66965f0fb8378631a62ee066bf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-03 22:27:40 +0530

    Moved the code into separate class.

--- SampleConsoleApps/Day3.Sample.App/MainCls.cs
+++ SampleConsoleApps/Day3.Sample.App/MainCls.cs
@@ -7,19 +7,7 @@ namespace Day3.Sample.App
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the number of Elements:");
-            var numberOfElements = int.Parse(Console.ReadLine().Trim());
-            var arrayElements = new int[numberOfElements];
-            for (var iCtr = 0; iCtr < numberOfElements; iCtr++)
-            {
-                Console.WriteLine($"Enter the {iCtr+1} Element:");
-                arrayElements[iCtr] = int.Parse(Console.ReadLine().Trim());
-            }
-
-            for (var iCtr = numberOfElements-1; iCtr >= 0; iCtr--)
-            {
-                Console.WriteLine($"{iCtr + 1} Element: {arrayElements[iCtr]}");
-            }
+            SingleDArrayReverse.Run();
 
             ArrayDemo2.Run();
 
---
+++ SampleConsoleApps/Day3.Sample.App/SingleDArrayReverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day3.Sample.App
+{
+    public class SingleDArrayReverse
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Enter the number of Elements:");
+            var numberOfElements = int.Parse(Console.ReadLine().Trim());
+            var arrayElements = new int[numberOfElements];
+            for (var iCtr = 0; iCtr < numberOfElements; iCtr++)
+            {
+                Console.WriteLine($"Enter the {iCtr + 1} Element:");
+                arrayElements[iCtr] = int.Parse(Console.ReadLine().Trim());
+            }
+
+            for (var iCtr = numberOfElements - 1; iCtr >= 0; iCtr--)
+            {
+                Console.WriteLine($"{iCtr + 1} Element: {arrayElements[iCtr]}");
+            }
+        }
+    }
+}
