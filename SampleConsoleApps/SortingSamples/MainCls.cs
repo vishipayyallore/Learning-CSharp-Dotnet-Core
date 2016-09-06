b979b12b162f5744cc95291ec9bc621e04542612
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-06 17:01:24 +0530

    Added New Project for Sorting Programs

---
+++ SampleConsoleApps/SortingSamples/MainCls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortingSamples
+{
+
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\nPress any key ...");
+            Console.ReadKey();
+        }
+    }
+
+}
