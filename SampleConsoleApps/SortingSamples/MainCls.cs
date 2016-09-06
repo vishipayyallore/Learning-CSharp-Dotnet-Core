3868fc5c90a64ef601ef29456c79f3f817b5672e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-06 17:01:59 +0530

    A

--- SampleConsoleApps/SortingSamples/MainCls.cs
+++ SampleConsoleApps/SortingSamples/MainCls.cs
@@ -5,12 +5,14 @@ namespace SortingSamples
 
     public class MainCls
     {
+
         public static void Main(string[] args)
         {
 
             Console.WriteLine("\n\nPress any key ...");
             Console.ReadKey();
         }
+
     }
 
 }
