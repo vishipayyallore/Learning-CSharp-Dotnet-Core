32275542484eeb8b551add2e4ae66cd50cdb7890
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-10 22:24:00 +0530

    Selection Sort Continuation

--- SampleConsoleApps/SortingSamples/MainCls.cs
+++ SampleConsoleApps/SortingSamples/MainCls.cs
@@ -10,8 +10,12 @@ namespace SortingSamples
         public static void Main(string[] args)
         {
 
-            var arrayData = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            //Selection Sort. {76 45 92 67 89 12 99 | 7, 4, 5, 2 | 4 83 9 6 34 45}
 
+            //Selection Sort
+            SelectionSort.Run();
+
+            //Bubble Sort
             BubbleSort.Run();
 
             Console.WriteLine("\n\nPress any key ...");
--- SampleConsoleApps/SortingSamples/SelectionSort.cs
+++ SampleConsoleApps/SortingSamples/SelectionSort.cs
@@ -7,5 +7,26 @@ namespace SortingSamples
 {
     public class SelectionSort
     {
+        public static void Run()
+        {
+            //Selection Sort. {76 45 92 67 89 12 99 | 7, 4, 5, 2 | 4 83 9 6 34 45}
+            var arrayData = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            for (var iCtr = 0; iCtr < arrayData.Length - 1; iCtr++)
+            {
+                var itemIndex = iCtr;
+                for (var jCtr = iCtr; jCtr < arrayData.Length; jCtr++)
+                {
+                    if (arrayData[jCtr] < arrayData[itemIndex])
+                    {
+                        itemIndex = jCtr;
+                    }
+                }
+                var temp = arrayData[iCtr];
+                arrayData[iCtr] = arrayData[itemIndex];
+                arrayData[itemIndex] = temp;
+                Console.WriteLine($"Pass {iCtr + 1}: {string.Join(" ", arrayData)}");
+            }
+            Console.WriteLine($"\nSelection Sorted: {string.Join(" ", arrayData)}");
+        }
     }
 }
