7ed4807a6d70f2e3d12c78519263621e04a2e888
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-12 01:26:46 +0530

    A

--- SampleConsoleApps/SortingSamples/InsertionSort.cs
+++ SampleConsoleApps/SortingSamples/InsertionSort.cs
@@ -1,6 +1,35 @@
-﻿namespace SortingSamples
+﻿using System;
+using System.Linq;
+
+namespace SortingSamples
 {
     public class InsertionSort
     {
+        public static void Run()
+        {
+            //Selection Sort. {76 45 92 67 89 12 99 | 7 4 5 2 | 4 83 9 6 34 45}
+            var arrayData = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+            for (var iCtr = 1; iCtr < arrayData.Length - 1; iCtr++)
+            {
+                var jCtr = iCtr;
+                while (jCtr > 0)
+                {
+                    if (arrayData[jCtr - 1] > arrayData[jCtr])
+                    {
+                        var temp = arrayData[jCtr];
+                        arrayData[jCtr] = arrayData[jCtr - 1];
+                        arrayData[jCtr - 1] = temp;
+                        jCtr--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine($"Pass {iCtr + 1}: {string.Join(" ", arrayData)}");
+            }
+            Console.WriteLine($"\nBubble Sorted: {string.Join(" ", arrayData)}\n");
+        }
     }
 }
--- SampleConsoleApps/SortingSamples/MainCls.cs
+++ SampleConsoleApps/SortingSamples/MainCls.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SortingSamples
 {
+
+    public class Product
+    {
+        public Product() { }
+
+        public int DayId { get; set; }
+        public int RowId { get; set; }
+        public int UserId { get; set; }
+        public int ObjectId { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class Sales
+    {
+        public List<Product> Products;
+    }
 
     public class MainCls
     {
@@ -10,29 +27,91 @@ namespace SortingSamples
         public static void Main(string[] args)
         {
 
-            //Selection Sort. {76 45 92 67 89 12 99 | 7 4 5 2 | 4 83 9 6 34 45}
-            var arrayData = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var salesList = new Sales {Products = new List<Product>()};
 
-            for (var iCtr = 1; iCtr < arrayData.Length - 1; iCtr++)
+            var firstLineInput = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            if (firstLineInput.Length != 3)
             {
-                var jCtr = iCtr;
-                while (jCtr > 0)
-                {
-                    if (arrayData[jCtr - 1] > arrayData[jCtr])
-                    {
-                        var temp = arrayData[jCtr];
-                        arrayData[jCtr] = arrayData[jCtr - 1];
-                        arrayData[jCtr - 1] = temp;
-                        jCtr--;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                Console.WriteLine($"Pass {iCtr + 1}: {string.Join(" ", arrayData)}");
+                Console.WriteLine("Invalid number of inputs..");
+                return;
             }
-            Console.WriteLine($"\nBubble Sorted: {string.Join(" ", arrayData)}\n");
+
+            var numberOfDays = firstLineInput[0];
+            if ((numberOfDays < 1) || (numberOfDays > 100))
+            {
+                Console.WriteLine("Invalid number of Days..");
+                return;
+            }
+            var numberofUsers = firstLineInput[1];
+            if ((numberofUsers < 1) || (numberofUsers > 100))
+            {
+                Console.WriteLine("Invalid number of Users..");
+                return;
+            }
+            var numberOfObjects = firstLineInput[2];
+            if ((numberOfObjects < 1) || (numberOfObjects > 100))
+            {
+                Console.WriteLine("Invalid number of Objects..");
+                return;
+            }
+
+
+            for (var days = 1; days <= numberOfDays; days++)
+            {
+                var numberOfRows = int.Parse(Console.ReadLine().Trim());
+                if ((numberOfRows < 1) || (numberOfRows > 20))
+                {
+                    Console.WriteLine("Invalid number of Rows..");
+                    days--;
+                    continue;
+                }
+
+                for (var rows = 1; rows <= numberOfRows; rows++)
+                {
+                    var dataElements = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+                    if (dataElements.Length != 3)
+                    {
+                        Console.WriteLine("Invalid number of inputs for data Elements..");
+                        rows--;
+                        continue;
+                    }
+
+                    var userId = dataElements[0];
+                    if ((userId < 1) || (userId > numberofUsers))
+                    {
+                        Console.WriteLine("Invalid User Id..");
+                        rows--;
+                        continue;
+                    }
+                    var objectId = dataElements[1];
+                    if ((objectId < 1) || (objectId > numberOfObjects))
+                    {
+                        Console.WriteLine("Invalid Object Id..");
+                        rows--;
+                        continue;
+                    }
+                    var rating = dataElements[2];
+                    if ((rating < 0) || (rating > 100))
+                    {
+                        Console.WriteLine("Invalid Rating..");
+                        rows--;
+                        continue;
+                    }
+
+                    var product = new Product {DayId = days, RowId = rows, UserId = userId, ObjectId = objectId, Rating = rating};
+                    salesList.Products.Add(product);
+                }
+            }
+
+            var highestAverage = salesList.Products.GroupBy(t => new { ObjectId = t.ObjectId })
+                .Select(g => new {Average = g.Average(r => r.Rating), ObjectId=g.Key.ObjectId});
+            var output = highestAverage.OrderByDescending(r => r.Average).First();
+
+            var numberOfTimes = salesList.Products.GroupBy(t => new { ObjectId = t.ObjectId })
+                .Select(g => new { Count = g.Count(), ObjectId = g.Key.ObjectId });
+            var output1 = numberOfTimes.OrderByDescending(r => r.Count).First();
+
+            Console.WriteLine($"{output.ObjectId} {output1.ObjectId}");
 
             //Selection Sort
             SelectionSort.Run();
