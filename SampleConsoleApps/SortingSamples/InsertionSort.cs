e721f85d48aba117e82c1da0e88804d6114c3433
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-13 20:04:40 +0530

    Insertion Sort

--- SampleConsoleApps/SortingSamples/InsertionSort.cs
+++ SampleConsoleApps/SortingSamples/InsertionSort.cs
@@ -3,14 +3,23 @@ using System.Linq;
 
 namespace SortingSamples
 {
+
+    /*
+        Step 1 − If it is the first element, it is already sorted. return 1;
+        Step 2 − Pick next element
+        Step 3 − Compare with all elements in the sorted sub-list
+        Step 4 − Shift all the elements in the sorted sub-list that is greater than the value to be sorted
+        Step 5 − Insert the value
+        Step 6 − Repeat until list is sorted
+    */
     public class InsertionSort
     {
         public static void Run()
         {
-            //Selection Sort. {76 45 92 67 89 12 99 | 7 4 5 2 | 4 83 9 6 34 45}
+            //Selection Sort. {76 45 92 67 89 12 99 | 7 4 5 2 | 34 4 83 9 6 45}
             var arrayData = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
 
-            for (var iCtr = 1; iCtr < arrayData.Length - 1; iCtr++)
+            for (var iCtr = 1; iCtr < arrayData.Length; iCtr++)
             {
                 var jCtr = iCtr;
                 while (jCtr > 0)
@@ -20,14 +29,10 @@ namespace SortingSamples
                         var temp = arrayData[jCtr];
                         arrayData[jCtr] = arrayData[jCtr - 1];
                         arrayData[jCtr - 1] = temp;
-                        jCtr--;
                     }
-                    else
-                    {
-                        break;
-                    }
+                    jCtr--;
                 }
-                Console.WriteLine($"Pass {iCtr + 1}: {string.Join(" ", arrayData)}");
+                Console.WriteLine($"Pass {iCtr}: {string.Join(" ", arrayData)}");
             }
             Console.WriteLine($"\nBubble Sorted: {string.Join(" ", arrayData)}\n");
         }
--- SampleConsoleApps/SortingSamples/MainCls.cs
+++ SampleConsoleApps/SortingSamples/MainCls.cs
@@ -9,6 +9,10 @@ namespace SortingSamples
 
         public static void Main(string[] args)
         {
+            //Sample Data: {76 45 92 67 89 12 99 | 7 4 5 2 | 34 4 83 9 6 45}
+
+            //Insertion Sort.
+            InsertionSort.Run();
 
             //Selection Sort
             SelectionSort.Run();
