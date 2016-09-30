859351cb0c222a4d8fa16955918ce6c290edb78a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-30 23:05:12 +0530

    New Project for sample programs.

---
+++ 30Sep2016/30Sep2016.App/MainCls.cs
@@ -0,0 +1,16 @@
+﻿
+using static System.Console;
+
+namespace _30Sep2016.App
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = System.ConsoleColor.Cyan;
+            
+            WriteLine("\n\nPress any key ...");
+            ReadLine();
+        }
+    }
+}
