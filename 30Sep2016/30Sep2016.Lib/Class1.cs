7f7a58c59b0bed43d6449c6a173f3be5a2bf2d24
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-30 23:55:54 +0530

    .

---
+++ 30Sep2016/30Sep2016.Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _30Sep2016.Lib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ 30Sep2016/30Sep2016.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_30Sep2016.Lib")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ebbf7984-af0c-4c96-80e2-1d4938643982")]
