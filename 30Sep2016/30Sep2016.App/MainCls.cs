8de47389b2909f3615eceaa0021e0fdde978bc20
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-09-30 23:16:05 +0530

    .

--- 30Sep2016/30Sep2016.App/MainCls.cs
+++ 30Sep2016/30Sep2016.App/MainCls.cs
@@ -8,7 +8,10 @@ namespace _30Sep2016.App
         public static void Main(string[] args)
         {
             ForegroundColor = System.ConsoleColor.Cyan;
-            
+            const string output = "Hello .Net Core!!!";
+
+            WriteLine($"{output}");
+
             WriteLine("\n\nPress any key ...");
             ReadLine();
         }
