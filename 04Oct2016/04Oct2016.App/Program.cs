e861bfb1b13a25051e3863825a7bf6b0314ffee9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-04 23:56:47 +0530

    .

--- 04Oct2016/04Oct2016.App/Program.cs
+++ 04Oct2016/04Oct2016.App/Program.cs
@@ -8,7 +8,11 @@ namespace _04Oct2016.App
         public static void Main(string[] args)
         {
             ForegroundColor = System.ConsoleColor.Cyan;
-            
+
+            WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
             WriteLine("\n\nPress any key ...");
             ReadLine();
         }
