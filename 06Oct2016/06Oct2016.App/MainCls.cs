2384514f983e2fd0d5eb8a09c9fa70702799d8f5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2016-10-06 21:54:32 +0530

    .

---
+++ 06Oct2016/06Oct2016.App/MainCls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _06Oct2016.App
+{
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            ForegroundColor = System.ConsoleColor.Cyan;
+
+            WriteLine("\n\nPress any key ...");
+            ReadLine();
+        }
+    }
+}
---
+++ 06Oct2016/06Oct2016.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_06Oct2016.Lib")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12628782-ece8-4393-86d4-8b9a8463c9d6")]
